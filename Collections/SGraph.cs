628e7b706d9bb0429128ec0c8928cd8c9f8afe9c
gordon <1176784982@qq.com>
2022-08-29 22:49:48 +0800

    good

---
+++ Collections/SGraph.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ques.Collections
+{
+    class SGraph<T>//Simple Graph, based on Linked Vertex
+    {
+        Dictionary<string, LinkedVertex<T>> Vertices;
+        public int VertexNum{ get; private set; }
+        public SGraph()
+        {
+            Vertices = new Dictionary<string, LinkedVertex<T>>();
+            VertexNum = 0;
+        }
+
+        public void AddVertex(LinkedVertex<T> vertex)
+        {
+            VertexNum++;
+        }
+    }
+}
