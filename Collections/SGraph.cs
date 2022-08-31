02206f2fd02dd80a69f984f8727a28f6e44e9d52
gordon <1176784982@qq.com>
2022-08-31 18:00:50 +0800

    added something

--- Collections/SGraph.cs
+++ Collections/SGraph.cs
@@ -22,30 +22,33 @@ namespace Ques.Collections
             VertexNum++;
         }
 
-        public bool BFS(LinkedVertex<T> start,T target)
+        public int BFS(LinkedVertex<T> start,T target)//返回最短距离 -1表示
         {
-            List<LinkedVertex<T>> pendingQueue = new List<LinkedVertex<T>>();
-            Dictionary<int, bool> overQueue = new Dictionary<int, bool>();
-            pendingQueue.Add(start);
+            List<KeyValuePair<LinkedVertex<T>, int>> pendingQueue = new List<KeyValuePair<LinkedVertex<T>, int>>();
+            Dictionary<int, int> overQueue = new Dictionary<int, int>();//key : ID Value : Hierarchy
+            pendingQueue.Add(new KeyValuePair<LinkedVertex<T>, int>(start,0));
             while (pendingQueue.Count != 0)
             {
-                LinkedVertex<T> curVertex = pendingQueue[0];
-                if (overQueue.ContainsKey(curVertex.ID))
+                LinkedVertex<T> curVertex = pendingQueue[0].Key;
+                int hierarchy = pendingQueue[0].Value;
+                if (curVertex.Data.Equals(target))
                 {
-                    continue;
+                    return hierarchy;
                 }
-                if (pendingQueue[0].Data.Equals(target))
-                {
-                    return true;
-                }
-                overQueue.Add(pendingQueue[0].ID,true);
+                overQueue.Add(curVertex.ID,hierarchy);
                 pendingQueue.RemoveAt(0);
                 foreach (LinkedVertex<T> adjaVertex in curVertex.AdjaVertices)
                 {
-                    
+                    if (overQueue.ContainsKey(adjaVertex.ID)) continue;
+                    pendingQueue.Add(new KeyValuePair<LinkedVertex<T>, int>(adjaVertex,hierarchy+1));
                 }
             }
-            return false;
+            return -1;
+        }
+
+        public SGraph<int> getGraphSample()
+        {
+            return null;
         }
     }
 }
